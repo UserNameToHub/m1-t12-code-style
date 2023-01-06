44a0a1bfec6d38a5ddca418c160fd7bdbb672506
aleksandrgineyka <meinepostkam@gmail.com>
2023-01-06 16:17:41 +0300

    add code style

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,48 +1,41 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double calculateComplexPercentFunction(double a, double y, int d ) {
-       double pay = a * Math.pow((1 + y/ 12), 12 *d);
-          return getRandom(pay, 2);
+    double calculateComplexPercentFunction(double a, double y, int d) {
+        double pay = a * Math.pow((1 + y / 12), 12 * d);
+        return getRandom(pay, 2);
     }
 
-    double calculateSimplePercentFunction(double doubleAmount, double double_year_rate, int deposit_period) {
-         return getRandom(doubleAmount + doubleAmount * double_year_rate * deposit_period, 2);
+    double calculateSimplePercentFunction(double doubleAmount, double doubleYearRate, int depositPeriod) {
+        return getRandom(doubleAmount + doubleAmount * doubleYearRate * depositPeriod, 2);
     }
 
     double getRandom(double value, int places) {
-       double ScaLe = Math.pow(10, places);
-
-       return Math.round(value * ScaLe) / ScaLe;
+        double ScaLe = Math.pow(10, places);
+        return Math.round(value * ScaLe) / ScaLe;
     }
 
     void calculateInterestOnDeposit() {
-      int period;
-      int action;
+        int period;
+        int action;
 
-      Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сумму вклада в рублях:");
+        int amount = scanner.nextInt();
+        System.out.println("Введите срок вклада в годах:");
+        period = scanner.nextInt();
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        action = scanner.nextInt();
 
-      System.out.println("Введите сумму вклада в рублях:");
+        double outDoubleVar = 0;
 
-      int amount = scanner.nextInt();
+        if (action == 1) {
+            outDoubleVar = calculateSimplePercentFunction(amount, 0.06, period);
+        } else if (action == 2) {
+            outDoubleVar = calculateComplexPercentFunction(amount, 0.06, period);
+        }
 
-      System.out.println("Введите срок вклада в годах:");
-
-      period = scanner.nextInt();
-
-      System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
-
-      action = scanner.nextInt();
-
-      double outDoubleVar = 0;
-
-      if (action == 1) {
-          outDoubleVar = calculateSimplePercentFunction(amount, 0.06, period);
-      } else if (action == 2) {
-          outDoubleVar = calculateComplexPercentFunction(amount, 0.06, period);
-      }
-
-      System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar);
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar);
     }
 
     public static void main(String[] args) {
